4bf0bee672b718e29fa4d9fd09c8251440b7ef69
STM <startinemrc@gmail.com>
2018-11-02 13:16:24 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// use a boolean var to check if the element is already shrinked
+// var heroShrunk = false,
+// 	hero = $(".hero");
+
+// $(window).on('scroll', function() {
+//   if ($(this).scrollTop() > 1){
+//     // add class only once
+//     if(!heroShrunk) {
+// 		$(hero).addClass("shrunk");
+// 	    // $(".line-container").hide();
+// 	    // $("#enter").hide();
+// 	    $("#minHero").show();
+//     }
+//   }
+// });
