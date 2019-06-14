9dd195144de9928035b67452f361af575221518e
STM <startinmerc@gmail.com>
2019-06-14 10:39:03 +0100

    simplify portfolio navigation

--- index.js
+++ index.js
@@ -24,114 +24,47 @@ function letterRandomize(i) {
 const btns = document.querySelectorAll(".section-text button");
 const pjts = document.querySelectorAll(".portfolio-entry");
 
-btns[0].addEventListener("click", showAll);
+btns[0].addEventListener("click", showFirst);
 
-function showAll() {
-	pjts.forEach((project)=>{
-		project.style.display = "initial";
-		project.classList.remove("max");
-		project.classList.add("min");
-		project.querySelector(".min-max-toggle button").innerText = "Show Details";
-	});
+function showFirst() {
+	toMax(pjts[0]);
 	btns[0].classList.add("button-active");
 	btns[1].classList.remove("button-active");
 	btns[2].classList.remove("button-active");
-	btns[3].classList.remove("button-active");
+	toMin(pjts[1]);
+	toMin(pjts[2]);
 }
 
-btns[1].addEventListener("click", showFirst);
-pjts[0].querySelector(".min-max-toggle").addEventListener("click", (button)=>{
-	if (pjts[0].classList.contains("max")) {
-		showAll();
-
-	} else {
-		showFirst();
-	}
-});
-
-function showFirst() {
-	if(pjts[0].classList.contains("min")){
-		pjts[0].style.display = "initial";
-		pjts[0].classList.remove("min");
-		pjts[0].classList.add("max");
-		btns[1].classList.add("button-active");
-		btns[0].classList.remove("button-active");
-		btns[2].classList.remove("button-active");
-		btns[3].classList.remove("button-active");
-	}
-	pjts[1].style.display = "none";
-	pjts[1].classList.add("min");
-	pjts[2].style.display = "none";
-	pjts[2].classList.add("min");
-	changeToggeText(pjts[0]);
-}
-
-btns[2].addEventListener("click", showSecond);
-pjts[1].querySelector(".min-max-toggle").addEventListener("click", (button)=>{
-	if (pjts[1].classList.contains("max")) {
-		showAll();
-	} else {
-		showSecond();
-	}
-});
+btns[1].addEventListener("click", showSecond);
 
 function showSecond() {
-	if(pjts[1].classList.contains("min")){
-		pjts[1].style.display = "initial";
-		pjts[1].classList.remove("min");
-		pjts[1].classList.add("max");
-		btns[2].classList.add("button-active");
-		btns[0].classList.remove("button-active");
-		btns[1].classList.remove("button-active");
-		btns[3].classList.remove("button-active");
-	}
-	pjts[0].style.display = "none";
-	pjts[0].classList.add("min");
-	pjts[2].style.display = "none";
-	pjts[2].classList.add("min");
-	changeToggeText(pjts[1]);
+	toMax(pjts[1]);
+	btns[1].classList.add("button-active");
+	btns[0].classList.remove("button-active");
+	btns[2].classList.remove("button-active");
+	toMin(pjts[0]);
+	toMin(pjts[2]);
 };
 
-btns[3].addEventListener("click", showThird);
-pjts[2].querySelector(".min-max-toggle").addEventListener("click", (button)=>{
-	if (pjts[2].classList.contains("max")) {
-		showAll();
-	} else {
-		showThird();
-	}
-});
+btns[2].addEventListener("click", showThird);
 
 function showThird() {
-	if(pjts[2].classList.contains("min")){
-		pjts[2].style.display = "initial";
-		pjts[2].classList.remove("min");
-		pjts[2].classList.add("max");
-		btns[3].classList.add("button-active");
-		btns[0].classList.remove("button-active");
-		btns[1].classList.remove("button-active");
-		btns[2].classList.remove("button-active");
-	}
-	pjts[0].style.display = "none";
-	pjts[0].classList.add("min");
-	pjts[1].style.display = "none";
-	pjts[1].classList.add("min");
-	changeToggeText(pjts[2]);
+	toMax(pjts[2]);
+	btns[2].classList.add("button-active");
+	btns[0].classList.remove("button-active");
+	btns[1].classList.remove("button-active");
+	toMin(pjts[0]);
+	toMin(pjts[1]);
 };
 
-function changeToggeText(project) {
-	if(project.classList.contains("max")){
-		project.querySelector(".min-max-toggle button").innerText = "Hide Details";
-	} else {
-		project.querySelector(".min-max-toggle button").innerText = "Show Details";
-	}
+function toMin(project) {
+	project.classList.remove("max");
+	project.classList.add("min");
+	project.style.display = "none";
 }
 
-function toggleMinMax(project) {
-	if(project.classList.contains("max")){
-		project.classList.remove("max");
-		project.classList.add("min");
-	} else {
-		project.classList.remove("min");
-		project.classList.add("max");
-	}
+function toMax(project) {
+	project.style.display = "initial";
+	project.classList.remove("min");
+	project.classList.add("max")
 }
